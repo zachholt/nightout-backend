d2986aff566a2de994ea6e146d09f6f07e06cc73
Zach <zacjac96@icloud.com>
2025-03-24 21:10:07 -0500

    Update CI/CD workflow to set environment variables for PostgreSQL configuration and modify application-test.yaml to use environment variables for datasource settings. Add @Primary annotation to UserRepository for bean management.

--- src/main/java/com/zachholt/nightout/repositories/UserRepository.java
+++ src/main/java/com/zachholt/nightout/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.zachholt.nightout.repositories;
 
 import com.zachholt.nightout.models.User;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
+@Primary
 public interface UserRepository extends JpaRepository<User, Long> {
     /**
      * Find a user by their email address
